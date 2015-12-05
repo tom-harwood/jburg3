f1512990f59e565d5fc5c8c1ce30f1dc2ad537cc
Tom Harwood <tharwood@theworld.com>
2015-12-04 21:23:33 -0500

    Comment Production regarding hashCode and equals

--- src/java/jburg/Production.java
+++ src/java/jburg/Production.java
@@ -8,6 +8,14 @@ import java.util.Map;
 
 /**
  * A Production encodes a opcode(operand, operand) pattern match.
+ * Note that Production uses identity semantics for its hashCode()
+ * and equals() methods (i.e., it doesn't override Object), which
+ * is used by the State objects that contain aggregations of
+ * Production and Closure productions, which can create an unbounded
+ * number of candidate States that only differ in that their costs
+ * are increasing due to closures; these states don't contribute any
+ * novel information and so the State defines its hashCode and equals
+ * methods in terms of the Production map's hashCode() and equals().
  */
 public class Production<Nonterminal, NodeType>
 {
