dfec102364e694eb302822ed59aa4383b42099ed
Tom Harwood <tharwood@theworld.com>
2015-12-18 20:45:49 -0500

    Weed out redundant data structures

--- src/java/jburg/ProductionTable.java
+++ src/java/jburg/ProductionTable.java
@@ -10,12 +10,46 @@ import java.util.*;
  */
 public class ProductionTable<Nonterminal, NodeType>
 {
-    private List<PatternMatcher<Nonterminal, NodeType>>         patternMatchers = new ArrayList<PatternMatcher<Nonterminal, NodeType>>();;
-    private List<Closure<Nonterminal>>                          closures        = new ArrayList<Closure<Nonterminal>>();
-    private Set<Nonterminal>                                    nonterminals    = new TreeSet<Nonterminal>();
-    private Set<State<Nonterminal, NodeType>>                   states          = new HashSet<State<Nonterminal, NodeType>>();
-    public  List<State<Nonterminal,NodeType>>                   stateTable      = null;
-    private Map<NodeType, List<Operator<Nonterminal,NodeType>>> operators       = new TreeMap<NodeType, List<Operator<Nonterminal,NodeType>>>();
+    /**
+     * Patterns applicable to null pointers; these are assembled
+     * into a singleton state, with no operator, and the resulting
+     * state is applied to all null pointers.
+     */
+    private List<PatternMatcher<Nonterminal, NodeType>> nullProductions = new ArrayList<PatternMatcher<Nonterminal, NodeType>>();;
+
+    /**
+     * The State that "derives" all null pointers.
+     */
+    State<Nonterminal, NodeType>    nullState = null;
+
+    /**
+     * Closures from the specification in entry order.
+     */
+    private List<Closure<Nonterminal>>  closures = new ArrayList<Closure<Nonterminal>>();
+
+    /**
+     * Nonterminals from the specification; there may be
+     * multiple references to a nonterminal, and their
+     * order here is not important.
+     */
+    private Set<Nonterminal>    nonterminals = new TreeSet<Nonterminal>();
+
+    /**
+     * Unique states, computed by permuting all inputs to each operator.
+     */
+    private Set<State<Nonterminal, NodeType>> states = new HashSet<State<Nonterminal, NodeType>>();
+
+    /**
+     * States in entry order, used for faster lookup by state number
+     * and to emit states in their "natural" order.
+     */
+    private List<State<Nonterminal, NodeType>> statesInEntryOrder = new ArrayList<State<Nonterminal, NodeType>>();
+
+    /**
+     * Operators computed from the specification, keyed by the nonterminal they produce.
+     */
+    private Map<NodeType, List<Operator<Nonterminal,NodeType>>> operators =
+        new TreeMap<NodeType, List<Operator<Nonterminal,NodeType>>>();
 
     /**
      * RepresenterStates, keyed by themselves
@@ -25,23 +59,74 @@ public class ProductionTable<Nonterminal, NodeType>
         new HashMap<RepresenterState<Nonterminal,NodeType>, RepresenterState<Nonterminal,NodeType>>();
 
     /**
-     * Pattern matchers by node type.
+     * Pattern matchers by node type and nominal arity.
+     * A fixed-arity pattern matcher's nominal arity is its number of children;
+     * a variadic pattern matcher has entries in this list for every arity where
+     * it overlaps with a fixed-arity pattern matcher, plus one extra nominal arity
+     * where it does not overlap any fixed-arity pattern matcher that represents
+     * the variadic tail.
      */
     private Map<NodeType, List<PatternMatcher<Nonterminal,NodeType>>> patternMatchersByNodeType =
         new TreeMap<NodeType, List<PatternMatcher<Nonterminal, NodeType>>>();
 
-
-    // TODO: @SafeVarargs would be a better annotation,
-    // but that would require Java 1.7 or above.
-    @SuppressWarnings({"unchecked"})
-    public PatternMatcher<Nonterminal, NodeType> addPatternMatch(Nonterminal nt, NodeType nodeType, Method method, Nonterminal... childTypes)
+    /**
+     * Add a pattern-matching production to the grammar.
+     * @param nt            the nonterminal this production produces.
+     * @param nodeType      the node type of the root of the subtree matched.
+     * @param cost          the cost of this production.
+     * @param preCallback   the callback run before deriving the subtree's child nodes.
+     * @param postCallback  the callback run after deriving child nodes.
+     * @param childTypes    the nonterminals the subtree's children must be able to produce.
+     */
+    @SuppressWarnings({"unchecked"})// TODO: @SafeVarargs would be a better annotation, but that would require Java 1.7 or above.
+    public PatternMatcher<Nonterminal, NodeType> addPatternMatch(Nonterminal nt, NodeType nodeType, int cost, Method preCallback, Method postCallback, Nonterminal... childTypes)
     {
-        PatternMatcher<Nonterminal,NodeType> result = new PatternMatcher<Nonterminal,NodeType>(nt, nodeType, 1, method, childTypes);
+        assert preCallback == null; // not ready for this
+        PatternMatcher<Nonterminal,NodeType> patternMatcher = new PatternMatcher<Nonterminal,NodeType>(nt, nodeType, cost, postCallback, childTypes);
         nonterminals.add(nt);
-        addPatternMatcher(result);
-        return result;
+        getPatternsForNodeType(nodeType).add(patternMatcher);
+
+        // Add an Operator to handle this pattern, if one is not already present.
+        // TODO: Handle variadic productions by propagating the "variadic" property
+        // forward through the operator list; this means the intermediate operators
+        // cannot be null, they must be present.
+        if (getOperator(nodeType, patternMatcher.size()) == null) {
+
+            if (!operators.containsKey(nodeType)) {
+                operators.put(nodeType, new ArrayList<Operator<Nonterminal,NodeType>>());
+            }
+
+            List<Operator<Nonterminal,NodeType>> ops = operators.get(nodeType);
+
+            if (ops.size() < patternMatcher.size() + 1) {
+                ops.addAll(Collections.nCopies((patternMatcher.size() + 1) - ops.size(), (Operator<Nonterminal,NodeType>)null));
+            }
+
+            ops.set(patternMatcher.size(), new Operator<Nonterminal,NodeType>(nodeType, patternMatcher.size()));
+        }
+
+        return patternMatcher;
     }
 
+    /**
+     * Add a pattern-matching production to the grammar, with unit cost.
+     * @param nt            the nonterminal this production produces.
+     * @param nodeType      the node type of the root of the subtree matched.
+     * @param postCallback  the callback run after deriving the child nodes.
+     * @param childTypes    the nonterminals the subtree's children must be able to produce.
+     */
+    @SuppressWarnings({"unchecked"})// TODO: @SafeVarargs would be a better annotation, but that would require Java 1.7 or above.
+    public PatternMatcher<Nonterminal, NodeType> addPatternMatch(Nonterminal nt, NodeType nodeType, Method postCallback, Nonterminal... childTypes)
+    {
+        return addPatternMatch(nt, nodeType, 1, null, postCallback, childTypes);
+    }
+
+    /**
+     * Add a closure to the grammar, with unit cost.
+     * @param targetNt      the nonterminal this closure produces.
+     * @param sourceNt      the nonterminal that must be produced before this closure runs.
+     * @param postCallback  the callback method run after the method that produces the source nonterminal completes.
+     */
     public Closure<Nonterminal> addClosure(Nonterminal targetNt, Nonterminal sourceNt, Method method)
     {
         Closure<Nonterminal> closure = new Closure<Nonterminal>(targetNt, sourceNt, method);
@@ -49,17 +134,27 @@ public class ProductionTable<Nonterminal, NodeType>
         return closure;
     }
 
+    /**
+     * Add a closure to the grammar, with unit cost and no callbacks.
+     */
     public void addClosure(Nonterminal targetNt, Nonterminal sourceNt)
     {
         addClosure(targetNt, sourceNt, null);
     }
 
+    /**
+     * Generate the states and transition tables for a grammar.
+     * @post states contains all state entries for the grammar,
+     * and each operator's transition table contains mappings
+     * to the corresponding states.
+     * TODO: explain this in more detail.
+     */
     public void generateStates()
     {
         Queue<State<Nonterminal, NodeType>> worklist = generateLeafStates();
 
         while (worklist.peek() != null) {
-            
+
             State<Nonterminal,NodeType> state = worklist.remove();
 
             for (List<Operator<Nonterminal,NodeType>> opList: operators.values()) {
@@ -74,6 +169,45 @@ public class ProductionTable<Nonterminal, NodeType>
                 }
             }
         }
+    }
+
+    /**
+     * Add a production to act on a null pointer.
+     * @param nt            the nonterminal this production produces.
+     * @param cost          the cost of this production.
+     * @param postCallback  the callback run by this production.
+     */
+    public Production<Nonterminal> addNullPointerProduction(Nonterminal nt, int cost, Method postCallback)
+    {
+        /*
+        Production<Nonterminal> np = new NullPointerProduction(nt, cost, postCallback);
+        nullProductions.add(np);
+        return np;
+        */
+        return null;
+    }
+
+    /**
+     * Get the singleton state that specifies valid transitions for a null pointer.
+     * If the grammar did not record any null productions, then this state is empty.
+     */
+    State<Nonterminal, NodeType> getNullState()
+    {
+        if (nullState == null) {
+
+            nullState = new State<Nonterminal, NodeType>();
+            for (PatternMatcher<Nonterminal, NodeType> p: nullProductions) {
+                if (p.ownCost < nullState.getCost(p.target)) {
+                    nullState.setPatternMatcher(p, p.ownCost);
+                }
+            }
+
+            closure(nullState);
+            State<Nonterminal, NodeType> result = addState(nullState);
+            assert result == nullState;
+        }
+
+        return nullState;
     }
 
     private void closure(State<Nonterminal,NodeType> state)
@@ -104,18 +238,24 @@ public class ProductionTable<Nonterminal, NodeType>
         Queue<State<Nonterminal, NodeType>> result = new ArrayDeque<State<Nonterminal, NodeType>>();
 
         for (NodeType nodeType: operators.keySet()) {
-            State<Nonterminal, NodeType> state = new State<Nonterminal, NodeType>(nodeType);
+            Operator<Nonterminal, NodeType> leafOperator = getOperator(nodeType, 0);
 
-            for (PatternMatcher<Nonterminal, NodeType> p: patternMatchers) {
-                if (p.nodeType == nodeType && p.isLeaf() && p.ownCost < state.getCost(p.target)) {
-                    state.setPatternMatcher(p, p.ownCost);
+            if (leafOperator != null) {
+                State<Nonterminal, NodeType> state = new State<Nonterminal, NodeType>(nodeType);
+
+                for (PatternMatcher<Nonterminal, NodeType> p: getPatternsForNodeType(nodeType)) {
+                    if (p.isLeaf() && p.ownCost < state.getCost(p.target)) {
+                        state.setPatternMatcher(p, p.ownCost);
+                    }
                 }
-            }
 
-            if (state.size() > 0) {
-                closure(state);
-                result.add(addState(state));
-                operators.get(nodeType).get(0).setLeafState(state);
+                // The set of non-empty leaf states is the basis for
+                // computing the transition tables.
+                if (state.size() > 0) {
+                    closure(state);
+                    result.add(addState(state));
+                    leafOperator.setLeafState(state);
+                }
             }
 
         }
@@ -138,7 +278,7 @@ public class ProductionTable<Nonterminal, NodeType>
                 for (List<RepresenterState<Nonterminal,NodeType>> repSet: generatePermutations(op, i, pState)) {
                 }
                 */
-                
+
                 List<RepresenterState<Nonterminal,NodeType>> prefix = new ArrayList<RepresenterState<Nonterminal,NodeType>>();
                 permute(op, 0, i, pState, prefix, workList);
             }
@@ -240,40 +380,20 @@ public class ProductionTable<Nonterminal, NodeType>
         }
     }
 
-    private void addPatternMatcher(PatternMatcher<Nonterminal,NodeType> patternMatcher)
-    {
-        NodeType nodeType = patternMatcher.nodeType;
-
-        patternMatchers.add(patternMatcher);
-        getPatternsForNodeType(nodeType).add(patternMatcher);
-        
-        if (!operators.containsKey(nodeType)) {
-            operators.put(nodeType, new ArrayList<Operator<Nonterminal,NodeType>>());
-        }
-
-        List<Operator<Nonterminal,NodeType>> ops = operators.get(nodeType);
-
-        if (ops.size() < patternMatcher.size() + 1) {
-            ops.addAll(Collections.nCopies((patternMatcher.size() + 1) - ops.size(), (Operator<Nonterminal,NodeType>)null));
-        }
-
-        ops.set(patternMatcher.size(), new Operator<Nonterminal,NodeType>(nodeType, patternMatcher.size()));
-    }
-
     private State<Nonterminal, NodeType> addState(State<Nonterminal, NodeType> state)
     {
         if (this.states.add(state)) {
             state.number = this.states.size();
+            statesInEntryOrder.add(state);
             return state;
         } else {
-            Iterator<State<Nonterminal,NodeType>> it = this.states.iterator();
-            while (it.hasNext()) {
-                State<Nonterminal,NodeType> s = it.next();
+            for (State<Nonterminal,NodeType> s: states) {
                 if (state.hashCode() == s.hashCode() && state.equals(s)) {
                     return s;
                 }
             }
         }
+        // If we ever get here, there is probably a bug in the State's hashCode() or equals() methods.
         throw new IllegalStateException(String.format("State %s not added and not present",state));
     }
 
@@ -286,32 +406,42 @@ public class ProductionTable<Nonterminal, NodeType>
         return repStates.get(rs);
     }
 
-    public State<Nonterminal, NodeType> getState(int stateNumber) {
-        if (stateNumber < 0) {
-            throw new IllegalArgumentException("Unlabled node");
-        } else {
-            return getStateTable().get(stateNumber-1);
+    /**
+     * Get a state by number.
+     */
+    State<Nonterminal, NodeType> getState(int stateNumber) {
+
+        if (stateNumber < 1 || stateNumber > statesInEntryOrder.size()) {
+            throw new IllegalArgumentException(String.format("State number %d is out of range 1..%d", stateNumber, states.size()));
         }
+
+        return statesInEntryOrder.get(stateNumber-1);
     }
 
-    private synchronized List<State<Nonterminal, NodeType>> getStateTable()
+
+    /**
+     * Get the Operator that handles a nodeType(nt*) tuple.
+     * @param nodeType  the node type of interest.
+     * @param arity     the actual arity of the subtree.
+     */
+    Operator<Nonterminal, NodeType> getOperator(NodeType nodeType, int arity)
     {
-        if (this.stateTable == null) {
-            this.stateTable = Collections.list(Collections.enumeration(states));
-            Collections.sort(stateTable);
+        List<Operator<Nonterminal, NodeType>> opsForNodeType = operators.get(nodeType);
+
+        // TODO: Check the final operator for variadics.
+        if (opsForNodeType != null && opsForNodeType.size() > arity) {
+            return opsForNodeType.get(arity);
         }
 
-        return stateTable;
+        return null;
     }
-
     public void dump(java.io.PrintWriter out)
     throws java.io.IOException
     {
-
         out.printf("<burmDump date=\"%s\">\n", new Date());
 
         out.println("<stateTable>");
-        for (State<Nonterminal, NodeType> s: getStateTable()) {
+        for (State<Nonterminal, NodeType> s: statesInEntryOrder) {
             s.dump(out);
         }
         out.println("</stateTable>");
@@ -333,16 +463,5 @@ public class ProductionTable<Nonterminal, NodeType>
         out.println("</burmDump>");
 
         out.flush();
-    }
-
-    public Operator<Nonterminal, NodeType> getOperator(NodeType nodeType, int arity)
-    {
-        List<Operator<Nonterminal, NodeType>> opsForNodeType = operators.get(nodeType);
-
-        if (opsForNodeType != null && opsForNodeType.size() > arity) {
-            return opsForNodeType.get(arity);
-        }
-
-        return null;
     }
 }
