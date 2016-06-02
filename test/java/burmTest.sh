aed6fbe03d8d3a3dfe1950ea5c2c56b4f8180770
Tom Harwood <tharwood@theworld.com>
2016-06-02 06:44:59 -0400

    -c <dump class> and -p <templates> options

--- test/java/burmTest.sh
+++ test/java/burmTest.sh
@@ -3,17 +3,17 @@ JBURG_HOME=$SCRIPT_DIR/../..
 
 usage()
 {
-    echo "Usage: burmTest [-r] [-q] -g <grammar> -t <testcase>"
+    echo "Usage: burmTest [-r] [-q] -g <grammar> -t <testcase> [-d <dumpfile>] [-p <templates>]"
 }
 
-args=`getopt c:d:g:t:rq $*`
+args=`getopt c:d:g:m:p:t:rq $*`
 if [ $? -ne 0 ]
 then
     usage;
     exit 2
 fi
 
-MAINCLASS=Calculator
+MAINCLASS=TestRunner
 
 set -- $args
 
@@ -22,15 +22,28 @@ do
     case "$i"
     in
         -c)
+            DUMPCLASS="-classname $2";
+            shift;
+            shift;;
+        -m)
             MAINCLASS=$2;
             shift;
             shift;;
         -d)
             DUMPFILE="-dump $2";
+            if [ "$TEMPLATES" == "" ]
+            then
+                TEMPLATES="-templates xml.stg"
+            fi
             shift;
             shift;;
         -g)
             GRAMMAR=$2;
+            shift;
+            shift;;
+        -p)
+            TEMPLATES=$2;
+                TEMPLATES="-templates $2"
             shift;
             shift;;
         -q)
@@ -48,14 +61,4 @@ do
     esac
 done
 
-if [ "$MAINCLASS" = "Calculator" ]
-then
-    if [ "$GRAMMAR" = "" -o "$TESTCASE" = "" ]
-    then
-        echo You must specify -grammar and -testcase.
-        usage;
-        exit 2
-    fi
-fi
-
-java -cp $JBURG_HOME/lib/jburg.jar:$HOME/tools/antlr-4.5.1-complete.jar:classes $MAINCLASS -grammar $GRAMMAR $QUIET $RANDOMIZE $DUMPFILE $TESTCASE
+java -cp $JBURG_HOME/lib/jburg.jar:$HOME/tools/antlr-4.5.1-complete.jar:classes $MAINCLASS -grammar $GRAMMAR $QUIET $RANDOMIZE $DUMPFILE $TEMPLATES $TESTCASE $DUMPCLASS
